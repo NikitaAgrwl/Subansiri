9c6296183693ca9664e18d34d92d82f84e17871e
nikitaagarwal208@gmail.com <nikita@antarx.tech>
2022-08-24 01:25:12 +0530

    Initial commit: code structure defined

---
+++ components/navigation.js
@@ -0,0 +1,47 @@
+document.write(`
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+    <a class="navbar-brand ms-5 " href="#"><img src="./img/shared/swhLogo.png" height="60px"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav  me-5 ms-auto">
+            <li class="nav-item" >
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Administration</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Achievements</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Boarders</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Facilities</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li> -->
+        </ul>
+    </div>
+</div>
+</nav>
+`);
