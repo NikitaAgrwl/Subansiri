25be382a0b6a308c8687ee3d5e7d2f1c39855b58
anujaes <anuj.aes@gmail.com>
2022-08-26 18:04:31 +0530

    small changes

--- js/quotes.js
+++ js/quotes.js
@@ -10,7 +10,6 @@ authorName = document.querySelector(".author .name");
 function randomQuote(){
     // fetching random quotes from the API and parsing it into Javascript object
     fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
-        console.log(result);
         quoteText.innerText = result.content;
         authorName.innerText = result.author;
     });
