49f5c8cd2d12abea2c953e925bd394bd9d1c81b3
nikitaagarwal208@gmail.com <nikita@antarx.tech>
2022-08-26 23:33:21 +0530

    boarders page merged

---
+++ js/sources/boarders.js
@@ -0,0 +1,254 @@
+﻿var boarders = {
+    "floor1": [
+        { "room": "101", "name": "Meenakshi Das", "dept": "MBBT" },
+        { "room": "101", "name": "Rupa Deka ", "dept": "CSE" },
+        { "room": "102", "name": "Anju Chetry", "dept": "CSE" },
+        // { "room": "102",    "name": "BHUPALI GOSWAMI",           "dept": "SOCIOLOGY"},
+        { "room": "103", "name": "Maitreyee Phukan", "dept": "ECE" },
+        { "room": "103", "name": "Pritirekha Dutta", "dept": "ECE" },
+        { "room": "104", "name": "Elura Sonowal", "dept": "ECE" },
+        { "room": "104", "name": "Meghna Roy", "dept": "CSE" },
+        { "room": "105", "name": "Darshana Baruah", "dept": "CSE" },
+        // { "room": "105",     "name": "PRIYA DUTTA",              "dept": "MBBT"},
+        { "room": "106", "name": "Harshita Yadav", "dept": "CSE" },
+        // { "room": "106",     "name": "SUSHWETA KAR",             "dept": "BUSINESS ADMINISTRATION"},
+        { "room": "107", "name": "Sonia Raj Gurung", "dept": "CE" },
+        { "room": "107", "name": "Musuka Basumatary", "dept": "ECE" },
+        // { "room": "108",     "name": "SONAI DUTTA",              "dept": "CHEMICAL SCIENCES"},
+        // { "room": "108",     "name": "SUPRIYA JAHNAVI DAS",      "dept": "ECE"},
+        { "room": "109", "name": "Bandita Deka", "dept": "Physics" },
+        // { "room": "109",     "name": "SAGARIKA BARMAN",          "dept": "ECE"},
+        { "room": "110", "name": "Rebika Salam", "dept": "FET" },
+        { "room": "110", "name": "Dimpal Pathak", "dept": "MCJ" },
+        // { "room": "111", "name": "DIKSHITA BARUAH",                  "dept": "COMMERCE"},
+        { "room": "111", "name": "Ananya Das", "dept": "Mathematics" },
+        { "room": "112", "name": "Didriksha Saikia", "dept": "MCJ" },
+        { "room": "112", "name": "Sebanki Biswas ", "dept": "ECE" },
+        // {"room": "113", "name": "ANANYA BORKOTOKY",              "dept": "ECE"},
+        // { "room": "113",    "name": "Ambika Mohta",              "dept": "MBBT" },
+        { "room": "114", "name": "Sweta Chatterjee", "dept": "MCJ" },
+        { "room": "114", "name": "Arpita Deb", "dept": "MCJ" },
+        { "room": "115", "name": "Dimpi saikia", "dept": "EVS" },
+        { "room": "115", "name": "Alboni bora", "dept": "EVS" },
+        { "room": "116", "name": "Sushmita Das", "dept": "EVS" },
+        { "room": "116", "name": "Rashmi Roy ", "dept": "EVS" },
+        { "room": "117", "name": "Ojosmita B. Goswami", "dept": "CSE" },
+        { "room": "117", "name": "Nitumoni Neog", "dept": "CSE" },
+        { "room": "118", "name": "Alankrita Priyadarshini", "dept": "CSE" },
+        // { "room": "118", "name": "SHASWATI GOGOI",                    "dept": "MATHEMATICAL SCIENCES"},
+        // { "room": "119", "name": "TRIDISHA BORAH",                    "dept": "CIVIL"},
+        // { "room": "119", "name": "RAJASMITA SINGHA",                "dept": "EFL"},
+        // { "room": "120", "name": "CHAYANIKA GOGOI",                  "dept": "EFL "},
+        // { "room": "120", "name": "PRAKRITI DUTTA",                    "dept": "FET"},
+        { "room": "121", "name": "Bijita Singha", "dept": "CSE" },
+        // { "room": "121", "name": "LEENA DEVI GOGOI",                "dept": "EFL"},
+        { "room": "122", "name": "Christeena Borah", "dept": "FET" },
+        { "room": "122", "name": "Ambika Swargiary", "dept": "MECHANICAL" },
+        { "room": "123", "name": "Shraddha Kundu", "dept": "MATHEMATICS" },
+        { "room": "123", "name": "Rajnandanee Biswas", "dept": "CSE" },
+        { "room": "124", "name": "Riddhi Saikia", "dept": "CHEMICAL SCIENCES" },
+        { "room": "124", "name": "Ridom Risha Gogoi", "dept": "CHEMICAL SCIENCES" },
+        { "room": "125", "name": "Jemima Yasmin", "dept": "CE" },
+        { "room": "125", "name": "Diksha Dutta", "dept": "CE" },
+        // { "room": "126",     "name": "PRATISTITA BAISHYA",   "dept": "MCJ"},
+        { "room": "126", "name": "Ekha", "dept": "CSE" },
+        { "room": "127", "name": "Jenifa", "dept": "MBBT" },
+        { "room": "127", "name": "Prodipti Dutta", "dept": "MECHANICAL" },
+        { "room": "128", "name": "Dibyani Lahon", "dept": "MECHANICAL" },
+        { "room": "128", "name": "Riya Ahmed", "dept": "MBBT" },
+        { "room": "129", "name": "Priyadarshini", "dept": "FET " },
+        { "room": "129", "name": "Anjali Singh Basfor", "dept": "MBBT" },
+        { "room": "130", "name": "Banamaya Goyari", "dept": "MATHEMATICS" },
+        { "room": "130", "name": "Sikhashree Barman", "dept": "MATHEMATICS" },
+        { "room": "131", "name": "Koyel Ray", "dept": "MBBT" },
+        { "room": "131", "name": "Piyali Dey", "dept": "MECHANICAL" },
+        // { "room": "132",     "name": "SANDHYATORA SAIKIA",   "dept": "EFL"},
+        { "room": "132", "name": "Nikita Agarwal", "dept": "CSE" },
+        { "room": "132", "name": "Puja Acharjee", "dept": "CHEMISTRY" },
+        { "room": "133", "name": "Bikoshita Gogoi", "dept": "CSE" },
+        { "room": "133", "name": "Saphy", "dept": "CSE" }
+    ],
+    "floor2":
+        [
+            { "room": "201", "name": "Kashmiri Borah", "dept": "CSE" },
+            { "room": "201", "name": "Upasana Parasar ", "dept": "CHEMICAL SCIENCES" },
+            { "room": "202", "name": "Pritymala Basumatary", "dept": "ECE" },
+            { "room": "202", "name": "Barsa Rani Dey", "dept": "MBBT" },
+            { "room": "203", "name": "Aditi Gogoi ", "dept": "CSE" },
+            { "room": "203", "name": "Singamayum Firdosh Nesha ", "dept": "FET" },
+            { "room": "204", "name": "Trishna Mani Deka ", "dept": "MCJ" },
+            { "room": "204", "name": "Khusboo Das", "dept": "CSE" },
+            // { "room": "205",     "name": "SONAM DAS",                    "dept": "EVS"},
+            { "room": "205", "name": "Smitakshi Goswami", "dept": "Physics" },
+            { "room": "206", "name": "Prakriti Dutta", "dept": "FET" },
+            { "room": "206", "name": "Dorothi Hazarika", "dept": "ECE" },
+            { "room": "207", "name": "Risha Kalita", "dept": "CHEMICAL SCIENCES" },
+            { "room": "207", "name": "Baishali Gogoi", "dept": "ECE" },
+            { "room": "208", "name": "Raaya Zaman", "dept": "EFL" },
+            { "room": "208", "name": "Chayanika Pegu", "dept": "CHEMICAL SCIENCES" },
+            { "room": "209", "name": "Juli Rani Basumatary", "dept": "CIVIL" },
+            { "room": "209", "name": "Rezoom Chakma", "dept": "CSE" },
+            { "room": "210", "name": "Shreoshri Kashyap", "dept": "CIVIL" },
+            { "room": "210", "name": "Mayur Trishna Saikia", "dept": "CIVIL" },
+            { "room": "211", "name": "Prantika Ojah ", "dept": "EVS" },
+            { "room": "211", "name": "Bhavna Dutta", "dept": "EVS" },
+            { "room": "212", "name": "Zeenat Mazid", "dept": "MCJ" },
+            { "room": "212", "name": "Dimple Baro", "dept": "MCJ" },
+            { "room": "213", "name": "Pujashree Gogoi", "dept": "FET" },
+            { "room": "213", "name": "Rosy Saikia ", "dept": "CHEMICAL SCIENCES" },
+            { "room": "214", "name": "Sorokhaibom Romita Devi ", "dept": "FET" },
+            { "room": "214", "name": "Smriti Roy", "dept": "PHYSICS" },
+            { "room": "215", "name": "Kaumudi Saikia", "dept": "CHEMICAL SCIENCES" },
+            { "room": "215", "name": "Jonmoni Dutta", "dept": "PHYSICS" },
+            { "room": "216", "name": "Nikita Buragohain", "dept": "MCJ" },
+            { "room": "216", "name": "Shubhangi Saini", "dept": "MBBT" },
+            { "room": "217", "name": "Yasmin Ara Begum", "dept": "EVS" },
+            { "room": "217", "name": "Anisha Hazarika", "dept": "EVS" },
+            { "room": "218", "name": "Trinita Singha Roy", "dept": "CHEMICAL SCIENCES" },
+            // { "room": "218",     "name": "KANKANA HALOI ",                       "dept": "CHEMICAL SCIENCES"},
+            { "room": "219", "name": "Afreen Hussain", "dept": "CHEMICAL SCIENCES" },
+            { "room": "219", "name": "Nurin Afzia", "dept": "FET" },
+            { "room": "220", "name": "Monica Deb", "dept": "UNKNOWN" },
+            { "room": "220", "name": "Sweta Das", "dept": "CIVIL" },
+            // { "room": "221",     "name": "DEEPIKA DAS",                     "dept": "FET"},
+            { "room": "221", "name": "Liza Hazarika", "dept": "MATHEMATICS" },
+            { "room": "222", "name": "Pinky Chetry", "dept": "CSE" },
+            { "room": "222", "name": "Sanahanbi Khangembam", "dept": "EVS" },
+            { "room": "223", "name": "Tridisha Sharma ", "dept": "EVS" },
+            { "room": "223", "name": "Bharoti Payeng", "dept": "EVS" },
+            { "room": "224", "name": "Pompi Talukdar ", "dept": "MECHANICAL" },
+            { "room": "224", "name": "Nitisha Kaushik ", "dept": "CSE" },
+            { "room": "225", "name": "Ruchika Bhoot", "dept": "CSE" },
+            { "room": "225", "name": "Pragya Das", "dept": "CIVIL" },
+            { "room": "226", "name": "Debajani Deori", "dept": "MECHANICAL" },
+            { "room": "226", "name": "Sukanya Chetia", "dept": "CHEMICAL SCIENCES" },
+            { "room": "227", "name": "Snigdha Das  ", "dept": "ECE" },
+            { "room": "227", "name": "Dorothy Das", "dept": "ECE" },
+            { "room": "228", "name": "Swapna Mondal", "dept": "MATHEMATICS" },
+            { "room": "228", "name": "Sreeja Saha", "dept": "MATHEMATICS" },
+            { "room": "229", "name": "Nipona Shill", "dept": "FET" },
+            { "room": "229", "name": "Meghali Das", "dept": "PHYSICS" },
+            // { "room": "230",     "name": "MOUSUMI  ",                 "dept": "MCJ"},
+            // { "room": "230",     "name": "PUJA DEVI",                 "dept": "HINDI"},
+            { "room": "231", "name": "Saumyashree", "dept": "MBBT" },
+            { "room": "231", "name": "Priyanka", "dept": "MBBT" },
+            { "room": "232", "name": "Sika Teronpi", "dept": "CHEMICAL SCIENCES" },
+            { "room": "232", "name": "Sibani Dhar", "dept": "CHEMICAL SCIENCES" },
+            { "room": "233", "name": "Sanghamitra Sarkar", "dept": "CSE" },
+            { "room": "233", "name": "Atifa Sarkar", "dept": "CHEMICAL SCIENCES" },
+            { "room": "234", "name": "Reshma Begum", "dept": "CHEMICAL SCIENCES" },
+            { "room": "234", "name": "Flora Emilia Basumatary", "dept": "CHEMICAL SCIENCES" }
+        ],
+    "floor3": 
+    [
+        { "room": "301", "name": "Chayanika Chetia", "dept": "MBBT" },
+        { "room": "301", "name": "Himakshi Brahma", "dept": "ECE" },
+        { "room": "302", "name": "Deepika Mandal", "dept": "EVS" },
+        { "room": "302", "name": "Mridula Dhar", "dept": "EVS" },
+        { "room": "303", "name": "Shikhamoni Nath", "dept": "MATHEMATICS" },
+        { "room": "303", "name": "Latashree Gogoi", "dept": "CSE" },
+        { "room": "304", "name": "Liza Dutta", "dept": "PHYSICS" },
+        { "room": "304", "name": "Priyanka Dey", "dept": "PHYSICS" },
+        { "room": "305", "name": "Piyali Das", "dept": "MCJ" },
+        { "room": "305", "name": "Himakshi Deka", "dept": "MATHEMATICS" },
+        { "room": "306", "name": "Rimjyoti Baishya ", "dept": "MATHEMATICS" },
+        { "room": "306", "name": "Himangshi Deka", "dept": "MATHEMATICS" },
+        { "room": "307", "name": "Trisha Khound", "dept": "ECE" },
+        { "room": "307", "name": "Elora Nath", "dept": "CHEMICAL SCIENCES" },
+        { "room": "308", "name": "Himashri Das", "dept": "CHEMICAL SCIENCES" },
+        { "room": "308", "name": "Reema Basumatary", "dept": "MATHEMATICS" },
+        { "room": "309", "name": "Shraddha Sinha", "dept": "CIVIL" },
+        { "room": "309", "name": "Sneha Talukdar", "dept": "CIVIL" },
+        { "room": "310", "name": "Kasturi Bora", "dept": "CSE" },
+        { "room": "310", "name": "Ishika Sharma", "dept": "CSE" },
+        { "room": "311", "name": "Parishmita Borah", "dept": "MATHEMATICS" },
+        { "room": "311", "name": "Lovita Das", "dept": "MATHEMATICS" },
+        { "room": "312", "name": "Kumkum Bhuyan", "dept": "CIVIL" },
+        { "room": "312", "name": "Ananya Thakur", "dept": "FET" },
+        { "room": "313", "name": "Smita Roy", "dept": "CIVIL" },
+        { "room": "313", "name": "Trishti Panging", "dept": "CIVIL" },
+        { "room": "314", "name": "Lubana Ishrat", "dept": "EVS" },
+        { "room": "314", "name": "Jumana Ishrat", "dept": "CHEMICAL SCIENCES" },
+        { "room": "315", "name": "Unnayana Gogoi", "dept": "CHEMICAL SCIENCES" },
+        // { "room": "315",     "name": "PUJA DAS",                   "dept": "PHYSICS"},
+        { "room": "316", "name": "Bhavna Choudhary", "dept": "CHEMICAL SCIENCES" },
+        { "room": "316", "name": "Richa Brahma", "dept": "CHEMICAL SCIENCES" },
+        { "room": "317", "name": "Karishma RayChoudhury", "dept": "ECE" },
+        { "room": "317", "name": "Aditi Das ", "dept": "MATHEMATICS" },
+        { "room": "318", "name": "Pallavi Prajapati", "dept": "CSE" },
+        { "room": "319", "name": "Simashree Saikia", "dept": "CHEMICAL SCIENCES" },
+        // { "room": "319",     "name": "BARNALI RAY",                     "dept": "MATHEMATICAL SCIENCES"},
+        { "room": "320", "name": "Pranamika Chetia", "dept": "CSE" },
+        { "room": "320", "name": "Rituparna Borah ", "dept": "ECE" },
+        { "room": "321", "name": "Isha Sasdeva", "dept": "MBBT" },
+        { "room": "321", "name": "Shilpa Chauhan", "dept": "MBBT" },
+        { "room": "322", "name": "Meghna Baruah", "dept": "CIVIL" },
+        // { "room": "322",     "name": "DIKSHITA HAZARIKA",                 "dept": "PHYSICS"},
+        { "room": "323", "name": "Trishna Das", "dept": "ENERGY" },
+        { "room": "323", "name": "Krishnamoni Gogoi", "dept": "ENERGY" },
+        { "room": "324", "name": "Bandita Rajkhowa", "dept": "MBBT" },
+        // { "room": "324",     "name": "SUBHANGEE PORASH",                   "dept": "EFL"},
+        { "room": "325", "name": "Sufee Sahin", "dept": "ECE" },
+        // { "room": "325",     "name": "SUKANYA SONOWAL ",                   "dept": "CSE"},
+        { "room": "326", "name": "Rinki Gogoi", "dept": "EE" },
+        // { "room": "326",     "name": "SONALI ROY",                       "dept": "CHEMICAL SCIENCES"},
+        { "room": "327", "name": "Mehjebin Wahid", "dept": "MATHEMATICS" },
+        { "room": "327", "name": "Bijay Laxmi Viswas", "dept": "CSE" },
+        { "room": "328", "name": "Nitumoni Sonowal", "dept": "EVS" },
+        { "room": "328", "name": "Khila Devi Khatiwoda", "dept": "EVS" },
+        { "room": "329", "name": "Aadrita Das", "dept": "EVS" },
+        { "room": "329", "name": "Barbie Duarah", "dept": "CSE" },
+        { "room": "330", "name": "Upasana Kalita", "dept": "CHEMICAL SCIENCES" },
+        { "room": "330", "name": "Kashyapi Dowerah", "dept": "CHEMICAL SCIENCES" },
+        { "room": "331", "name": "Sampriti Bora ", "dept": "PHYSICS" },
+        { "room": "331", "name": "Sabiba Ahmed", "dept": "PHYSICS" },
+        { "room": "332", "name": "Kankita Nath", "dept": "CSE" },
+        // { "room": "332",     "name": "KABITA BORA ",                   "dept": "EDUCATION"},
+        { "room": "333", "name": "Sukla Das", "dept": "FET" },
+        { "room": "333", "name": "Prena Mehta", "dept": "FET" },
+        { "room": "334", "name": "Ruplekha Bordoloi", "dept": "CHEMICAL SCIENCES" },
+        { "room": "334", "name": "Meethi Buragohain ", "dept": "PHYSICS" }
+    ]
+};
+
+// table one
+let listOne ='';
+let counterOne = 1;
+for (let item of boarders['floor1'].values()) {
+    listOne += `<tr>
+                <th scope="row">${counterOne}</th>
+                <td>${item['room']}</td>
+                <td>${item['name']}</td>
+                <td>${item['dept']}</td>
+            </tr>`;
+    counterOne = counterOne + 1;
+}
+document.getElementById("table-data-1").innerHTML = listOne;
+
+// table two
+let listTwo = '';
+let counterTwo = 1;
+for ( item of boarders['floor2'].values()) {
+    listTwo += `<tr>
+                <th scope="row">${counterTwo}</th>
+                <td>${item['room']}</td>
+                <td>${item['name']}</td>
+                <td>${item['dept']}</td>
+            </tr>`;
+    counterTwo = counterTwo + 1;
+}
+document.getElementById("table-data-2").innerHTML = listTwo;
+
+// table three
+let listThree = '';
+let counterThree = 1;
+for ( item of boarders['floor3'].values()) {
+    listThree += `<tr>
+                <th scope="row">${counterThree}</th>
+                <td>${item['room']}</td>
+                <td>${item['name']}</td>
+                <td>${item['dept']}</td>
+            </tr>`;
+    counterThree = counterThree + 1;
+}
+document.getElementById("table-data-3").innerHTML = listThree;
